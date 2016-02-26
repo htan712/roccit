3cf1c2c54e2e59bffbc983c795853206874c0c7c
Hock Tan <hock.tan712@gmail.com>
2016-02-26 11:58:06 -0800

    Session added, nav bar better

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   post 'users/confirm' => 'users#confirm'
+
+  resources :sessions, only: [:new, :create, :destroy]
   root 'welcome#index'
 
   get 'about' => 'welcome#about'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,5 +41,11 @@ RSpec.describe UsersController, type: :controller do
        expect(assigns(:user).password).to eq new_user_attributes[:password]
        expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
      end
+
+     it "logs the user in after sign up" do
+       post :create, user: new_user_attributes
+       expect(session[:user_id]).to eq assigns(:user).id
+     end
    end
+
 end
