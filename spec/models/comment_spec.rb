4fd1bc43b6df121a78cdff45c936087bd7f3a983
Hock Tan <hock.tan712@gmail.com>
2016-02-28 18:00:39 -0800

    added comment create/destroy with auth

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Comment, type: :model do
   let(:user) { User.create!(name: "Hock Tan", email: "jtan712@bloc.com", password: "password") }
   let(:topic) { Topic.create!(name: Faker::Hipster.sentence, description: Faker::Hipster.paragraph) }
   let(:post) { topic.posts.create!(title: Faker::Hipster.sentence, body: Faker::Hipster.paragraph, user: user) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
 
    describe "attributes" do
      it "responds to body" do
