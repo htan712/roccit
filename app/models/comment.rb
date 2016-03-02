2c58cb392ff199a9c52c1e2c456f49c18551b6fc
Hock Tan <hock.tan712@gmail.com>
2016-03-02 15:29:26 -0800

    renew sendgrid

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  after_create :send_favorite_emails
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
+
+  after_create :send_favorite_emails
 
   private
 
@@ -12,5 +13,5 @@ class Comment < ActiveRecord::Base
     post.favorites.each do |favorite|
       FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
     end
-  end
+   end
 end
